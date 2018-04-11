ed75d393c72af8df84f302a575e336ff15793856
jamesgourley <38119012+jamesgourley@users.noreply.github.com>
2018-04-11 10:56:35 +1000

    Update google-doc-url.js
    Modified google maps url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1BvoIC4HJLrtMcNd_xNAR-2NUjSfi4IUrNk_06buJNFo/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1XZleKfoV5R2lEKpcMH2dEtwQwolDCK4hrqEu6kfHsHY/edit#gid=0';
